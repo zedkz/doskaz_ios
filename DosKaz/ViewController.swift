8a23a5b9c327b474e386722059eefc110d82fb19
Yesbol Kulanbekov <esbolkz@yahoo.com>
2020-02-14 21:03:05 +0600

    create the xcode project

---
+++ DosKaz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DosKaz
+//
+//  Created by Yesbol Kulanbekov on 2/14/20.
+//  Copyright © 2020 zed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
