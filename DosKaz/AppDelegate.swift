9ac16133d480e6e1c8628547c76f03eaa805d9ca
Yesbol Kulanbekov <esbolkz@yahoo.com>
2020-02-23 21:43:41 +0600

    set the main interface in code

--- DosKaz/AppDelegate.swift
+++ DosKaz/AppDelegate.swift
@@ -14,8 +14,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
+		setRootViewController()
 		return true
+	}
+	
+	private func setRootViewController() {
+		window = UIWindow(frame: UIScreen.main.bounds)
+		window?.makeKeyAndVisible()
+		window?.rootViewController = UIViewController()
+		if #available(iOS 13.0, *) {
+			window?.overrideUserInterfaceStyle = .light
+		}
 	}
 
 	// MARK: UISceneSession Lifecycle
