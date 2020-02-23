d002dddd447378b36a1df8c2d5323baa562930eb
Yesbol Kulanbekov <esbolkz@yahoo.com>
2020-02-23 16:39:53 +0600

    add SharedCode xcframework

---
+++ Externals/SharedCodeFramework.xcframework/ios-x86_64-simulator/SharedCodeFramework.framework/Headers/SharedCodeFramework.h
@@ -0,0 +1,19 @@
+//
+//  SharedCodeFramework.h
+//  SharedCodeFramework
+//
+//  Created by Yesbol Kulanbekov on 5/9/19.
+//  Copyright © 2019 Lobster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SharedCodeFramework.
+FOUNDATION_EXPORT double SharedCodeFrameworkVersionNumber;
+
+//! Project version string for SharedCodeFramework.
+FOUNDATION_EXPORT const unsigned char SharedCodeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SharedCodeFramework/PublicHeader.h>
+
+
