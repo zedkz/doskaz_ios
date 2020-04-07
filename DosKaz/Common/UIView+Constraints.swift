f19e6280b6674484a2447305a39b80b228adfcf7
Yesbol Kulanbekov <esbolkz@yahoo.com>
2020-04-07 19:52:26 +0600

    update the autolayout wrapper

--- DosKaz/Common/UIView+Constraints.swift
+++ DosKaz/Common/UIView+Constraints.swift
@@ -6,8 +6,6 @@
 //  Copyright © 2020 zed. All rights reserved.
 //
 
-import SharedCodeFramework
-
 // TODO: Add to Shared
 // This is the improvement over autolayout wrapper
 // Example
@@ -16,12 +14,69 @@ import SharedCodeFramework
 //	.pin(my: .top, andOf: view, plus: 200)
 
 
+import UIKit
 
-public extension UIView {
+public enum Dimensions {
+	case width
+	case height
 	
-	var addConstraintsProgrammatically: UViewWrapper {
+	var keyPath: KeyPath<UIView, NSLayoutDimension> {
+		switch self {
+		case .width:
+			return \UIView.widthAnchor
+		case .height:
+			return \UIView.heightAnchor
+		}
+	}
+	
+}
+
+public enum YEdge {
+	case top
+	case bottom
+	case verticalCenter
+	case firstBaseline
+	case lastBaseline
+	
+	var keyPath: KeyPath<UIView, NSLayoutYAxisAnchor> {
+		switch self {
+		case .top:
+			return \UIView.topAnchor
+		case .bottom:
+			return \UIView.bottomAnchor
+		case .verticalCenter:
+			return \UIView.centerYAnchor
+		case .firstBaseline:
+			return \UIView.firstBaselineAnchor
+		case .lastBaseline:
+			return \UIView.lastBaselineAnchor
+		}
+	}
+	
+}
+
+public enum XEdge {
+	case leading
+	case trailing
+	case horizontalCenter
+	
+	var keyPath: KeyPath<UIView, NSLayoutXAxisAnchor> {
+		switch self {
+		case .leading:
+			return \UIView.leadingAnchor
+		case .trailing:
+			return \UIView.trailingAnchor
+		case .horizontalCenter:
+			return \UIView.centerXAnchor
+		}
+	}
+	
+}
+
+extension UIView {
+	var addConstraintsProgrammatically: Anchor {
 		translatesAutoresizingMaskIntoConstraints = false
-		return UViewWrapper(view: self)
+		return Anchor(view: self)
 	}
 	
 	var safeLayoutGuide: UILayoutGuide {
@@ -31,102 +86,275 @@ public extension UIView {
 			return layoutMarginsGuide
 		}
 	}
+}
+
+public class Anchor {
+	
+	let callingView: UIView
+	
+	var constraint: NSLayoutConstraint?
+	
+	init(view: UIView) {
+		self.callingView = view
+	}
 	
 }
 
-public struct UViewWrapper {
-	let view: UIView
+extension Anchor {
 	
-	private func activate(this constraint : Constraint) -> Self {
-		constraint(view).isActive = true
+	/// Common method for all other constraint makers with X and Y
+	private func constrain<Anchor, Axis>(
+		anchor myAnchor: KeyPath<UIView, Anchor>,
+		with relation: NSLayoutConstraint.Relation = .equal,
+		to otherViewsAnchor: KeyPath<UIView, Anchor>,
+		of view: UIView,
+		plus c: CGFloat
+	) -> Self where Anchor: NSLayoutAnchor<Axis> {
+		
+		let anchor = callingView[keyPath: myAnchor]
+		let otherAnchor = view[keyPath: otherViewsAnchor]
+		
+		var constraint: NSLayoutConstraint?
+		
+		switch relation {
+		case .equal:
+			constraint = anchor.constraint(equalTo: otherAnchor, constant: c)
+		case .greaterThanOrEqual:
+			constraint = anchor.constraint(greaterThanOrEqualTo: otherAnchor, constant: c)
+		case .lessThanOrEqual:
+			constraint = anchor.constraint(lessThanOrEqualTo: otherAnchor, constant: c)
+		@unknown default:
+			break
+		}
+		
+		constraint?.isActive = true
+		self.constraint = constraint
+		
 		return self
 	}
+	
+	private func constrain<LayoutDimension>(
+		dimension myDimension: KeyPath<UIView, LayoutDimension>,
+		with relation: NSLayoutConstraint.Relation,
+		to otherDimension: KeyPath<UIView, LayoutDimension>,
+		of view: UIView,
+		times x: CGFloat,
+		plus c: CGFloat
+	) -> Self where  LayoutDimension: NSLayoutDimension {
+		
+		let dimension = callingView[keyPath: myDimension]
+		let otherDimension = view[keyPath: otherDimension]
+		
+		var constraint: NSLayoutConstraint?
+		
+		switch relation {
+		case .equal:
+			constraint = dimension.constraint(equalTo: otherDimension, multiplier: x, constant: c)
+		case .greaterThanOrEqual:
+			constraint = dimension.constraint(greaterThanOrEqualTo: otherDimension, multiplier: x, constant: c)
+		case .lessThanOrEqual:
+			constraint = dimension.constraint(lessThanOrEqualTo: otherDimension, multiplier: x, constant: c)
+		@unknown default:
+			break
+		}
+		
+		constraint?.isActive = true
+		self.constraint = constraint
+		
+		return self
+	}
+	
 }
+
+/// XEdge
+extension Anchor {
 	
-extension UViewWrapper {
-	
-	/// Describes relation between Y anchors of two views
-	/// Example: 'pin(my: .top, to: .bottom, of: view, plus: 40)'
+	/// Describes relation between X anchors of two views
+	/// Example: 'set(my: .leading, .equal, to: .trailing, of: view, plus: 30)'
 	@discardableResult
-	func pin(
-		my yEdge: YEdge,
-		to viewsYEdge : YEdge,
+	public func set(
+		my xEdge: XEdge,
+		_ relation: NSLayoutConstraint.Relation,
+		to viewsXEdge : XEdge,
 		of view: UIView,
 		plus c: CGFloat = 0
 	) -> Self {
-		return activate(this: make(my: yEdge, .equal, to: viewsYEdge, of: view, plus: c))
+		return constrain(anchor: xEdge.keyPath, with: relation, to: viewsXEdge.keyPath, of: view, plus: c)
 	}
-	
-	
-	/// Describes relation between Y anchors of two views
-	/// Example: 'pin(my: .top, andOf: view, plus: 16)'
-	@discardableResult
-	func pin(
-		my yEdge: YEdge,
-		andOf view: UIView,
-		plus c: CGFloat = 0
-	) -> Self {
-		return pin(my: yEdge, to: yEdge, of: view, plus: c)
-	}
-	
 	
 	/// Describes relation between X anchors of two views
 	/// Example: 'pin(my: .leading, to: .trailing, of: view, plus: 40)'
 	@discardableResult
-	func pin(
+	public func pin(
 		my xEdge: XEdge,
 		to viewsXEdge : XEdge,
 		of view: UIView,
 		plus c: CGFloat = 0
 	) -> Self {
-		return activate(this:  make(my: xEdge, .equal, to: viewsXEdge, of: view, plus: c))
+		return constrain(anchor: xEdge.keyPath, with: .equal, to: viewsXEdge.keyPath, of: view, plus: c)
 	}
-	
 	
 	/// Describes relation between X anchors of two views
 	/// Example: 'pin(my: .leading, andOf: view, plus: 16)'
 	@discardableResult
-	func pin(
+	public func pin(
 		my xEdge: XEdge,
 		andOf view: UIView,
 		plus c: CGFloat = 0
 	) -> Self {
-		return pin(my: xEdge, to: xEdge, of: view, plus: c)
+		return constrain(anchor: xEdge.keyPath, with: .equal, to: xEdge.keyPath, of: view, plus: c)
 	}
 	
+	/// Pin edge to super view
+	/// Example: '.pinEdgeToSuper(my: .leading)'
+	@discardableResult
+	public func pinEdgeToSupers(
+		_ xEdge: XEdge,
+		plus c: CGFloat = 0
+	) -> Self {
+		return constrain(anchor: xEdge.keyPath, with: .equal, to: xEdge.keyPath, of: callingView.superview!, plus: c)
+	}
+	
+}
+
+/// YEdge
+extension Anchor {
+	
+	/// Describes relation between Y anchors of two views
+	/// Example: 'make(my: .bottom, .equal, to: .top, of: view, plus: 40)'
+	@discardableResult
+	public func set(
+		my yEdge: YEdge,
+		_ relation: NSLayoutConstraint.Relation,
+		to viewsYEdge : YEdge,
+		of view: UIView,
+		plus c: CGFloat = 0
+	) -> Self {
+		return constrain(anchor: yEdge.keyPath, with: relation, to: viewsYEdge.keyPath, of: view, plus: c)
+	}
+	
+	/// Describes relation between Y anchors of two views
+	/// Example: 'pin(my: .top, to: .bottom, of: view, plus: 40)'
+	@discardableResult
+	public func pin(
+		my yEdge: YEdge,
+		to viewsYEdge : YEdge,
+		of view: UIView,
+		plus c: CGFloat = 0
+	) -> Self {
+		return constrain(anchor: yEdge.keyPath, with: .equal, to: viewsYEdge.keyPath, of: view, plus: c)
+	}
+	
+	/// Describes relation between Y anchors of two views
+	/// Example: 'pin(my: .top, andOf: view, plus: 16)'
+	@discardableResult
+	public func pin(
+		my yEdge: YEdge,
+		andOf view: UIView,
+		plus c: CGFloat = 0
+	) -> Self {
+		return constrain(anchor: yEdge.keyPath, with: .equal, to: yEdge.keyPath, of: view, plus: c)
+	}
+	
+	/// Pin edge to super view
+	/// Example: '.pinEdgeToSuper(my: .leading)'
+	@discardableResult
+	public func pinEdgeToSupers(
+		_ yEdge: YEdge,
+		plus c: CGFloat = 0
+	) -> Self {
+		return constrain(anchor: yEdge.keyPath, with: .equal, to: yEdge.keyPath, of: callingView.superview!, plus: c)
+	}
+	
+}
+
+/// Dimension
+extension Anchor {
+	
+	/// Describes relation between dimension of two views
+	/// Example: 'make(my: .height, .greaterThanOrEqual, to: .width, of: view, times: 2, plus: 3)'
+	@discardableResult
+	public func set(
+		my dimension: Dimensions,
+		_ relation: NSLayoutConstraint.Relation ,
+		to viewsDimension : Dimensions,
+		of view: UIView,
+		times x: CGFloat = 1,
+		plus c: CGFloat = 0
+	) -> Self {
+		return constrain(dimension: dimension.keyPath, with: relation, to: viewsDimension.keyPath, of: view, times: x, plus: c)
+	}
+	
+	/// Describes constraint that will be equal to greater or less than a constant
+	/// Examples: 'set(my: .height, to: 22)'
+	/// 'set(my: .height, .greaterThanOrEqual, to: 23)'
+	/// 'set(my: .height, .lessThanOrEqual, to: 23)'
+	@discardableResult
+	public func set(
+		my dimension: Dimensions,
+		_ relation: NSLayoutConstraint.Relation = .equal,
+		to constant: CGFloat
+	) -> Self {
+		
+		let anchor = callingView[keyPath: dimension.keyPath]
+		
+		switch relation {
+		case .equal:
+			constraint = anchor.constraint(equalToConstant: constant)
+		case .greaterThanOrEqual:
+			constraint = anchor.constraint(greaterThanOrEqualToConstant: constant)
+		case .lessThanOrEqual:
+			constraint = anchor.constraint(lessThanOrEqualToConstant: constant)
+		@unknown default:
+			break
+		}
+		
+		constraint?.isActive = true
+		
+		return self
+		
+	}
+	
+	/// Describes relation between dimension of two views
+	/// Example: 'match(my: .height, and: .height, of: view, times: 2, plus: 3)'
+	@discardableResult
+	public func equate(
+		my dimension: Dimensions,
+		and viewsDimension : Dimensions,
+		of view: UIView,
+		times x: CGFloat = 1,
+		plus c: CGFloat = 0
+	) -> Self {
+		return constrain(dimension: dimension.keyPath, with: .equal, to: viewsDimension.keyPath, of: view, times: x, plus: c)
+	}
+	
+}
+
+/// Super view
+extension Anchor {
+	
+	/// Pin view to its superview safeArea
+	@discardableResult
+	public func pinToSuperSafeArea(
+		inset i: UIEdgeInsets = UIEdgeInsets(all: 0)
+	) -> Self {
+		let superview = callingView.superview!
+		return constrain(anchor: YEdge.top.keyPath, to: \.safeLayoutGuide.topAnchor, of: superview, plus: i.top)
+			.constrain(anchor: YEdge.bottom.keyPath, to: \.safeLayoutGuide.bottomAnchor, of: superview, plus: i.bottom)
+			.constrain(anchor: XEdge.leading.keyPath, to: \.safeLayoutGuide.leadingAnchor, of: superview, plus: i.left)
+			.constrain(anchor: XEdge.trailing.keyPath, to: \.safeLayoutGuide.trailingAnchor, of: superview, plus: i.right)
+	}
 	
 	/// Pin view to its superview
 	@discardableResult
-	public func pinToSuper(with insets: UIEdgeInsets = UIEdgeInsets(all: 0)) -> Self {
-		
-		self.pin(my: .top, andOf: view.superview!, plus: insets.top)
-			.pin(my: .bottom, andOf: view.superview!, plus: -insets.top)
-			.pin(my: .leading, andOf: view.superview!, plus: insets.top)
-			.pin(my: .trailing, andOf: view.superview!, plus: -insets.top)
-				
-		return self
+	public func pinToSuper(
+		inset i: UIEdgeInsets = UIEdgeInsets(all: 0)
+	) -> Self {
+		return pinEdgeToSupers(.leading, plus: i.left)
+			.pinEdgeToSupers(.trailing, plus: i.right)
+			.pinEdgeToSupers(.top, plus: i.top)
+			.pinEdgeToSupers(.bottom, plus: i.bottom)
 	}
-	
-	
-	// Need implementation
-	public func pinToSuperView(with layoutGuide: UILayoutGuide? = nil) -> Self { return self }
-	
-	
-	/// Pin view to its superview's safe area
-	@discardableResult
-	public func pinToSuperSafeArea(with insets: UIEdgeInsets = UIEdgeInsets(all: 0)) -> Self {
-		
-		let constraints =  [
-			view.topAnchor.constraint(equalTo: view.superview!.safeLayoutGuide.topAnchor, constant: insets.top ),
-			view.bottomAnchor.constraint(equalTo: view.superview!.safeLayoutGuide.bottomAnchor, constant: -insets.bottom ),
-			view.leadingAnchor.constraint(equalTo: view.superview!.safeLayoutGuide.leadingAnchor, constant: insets.left ),
-			view.trailingAnchor.constraint(equalTo: view.superview!.safeLayoutGuide.trailingAnchor, constant: -insets.right )
-		]
-
-		NSLayoutConstraint.activate(constraints)
-		
-		return self
-	}
-	
 	
 }
+
