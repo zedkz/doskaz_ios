5091db926f2ed9e2b3fc191491bf47c8089da9a7
Yesbol Kulanbekov <esbolkz@yahoo.com>
2020-04-21 14:37:29 +0600

    update setRootViewController

--- DosKaz/AppDelegate.swift
+++ DosKaz/AppDelegate.swift
@@ -21,10 +21,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	private func setRootViewController() {
 		window = UIWindow(frame: UIScreen.main.bounds)
 		window?.makeKeyAndVisible()
-		window?.rootViewController = MainTabBarViewController()
 		if #available(iOS 13.0, *) {
 			window?.overrideUserInterfaceStyle = .light
 		}
+		
+		let isUserIntroducedToApp = false
+		if isUserIntroducedToApp {
+			window?.rootViewController = MainTabBarViewController()
+		} else {
+			let introductionFlow = PortraitNavigationController(
+				rootViewController:
+				LanguageChoiceBuilder().assembleModule()
+			)
+			window?.rootViewController = introductionFlow
+		}
+		
 	}
 	
 	private func setMockViewController(_ viewController: UIViewController) {
