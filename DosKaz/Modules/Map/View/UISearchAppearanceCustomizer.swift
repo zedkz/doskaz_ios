749822bc3e58dbaeef46b50c8cbc7924b09c85d6
Yesbol Kulanbekov <esbolkz@yahoo.com>
2020-05-02 16:15:33 +0600

    update search field appearance

--- DosKaz/Modules/Map/View/UISearchAppearanceCustomizer.swift
+++ DosKaz/Modules/Map/View/UISearchAppearanceCustomizer.swift
@@ -11,10 +11,13 @@ import UIKit
 extension UISearchBar {
 	func customize() {
 		self.layer.shadowOffset = CGSize(width: 0, height: 0)
-		self.layer.shadowColor = UIColor(named: "CategoryPickerHeaderBackground")?.cgColor
+		self.layer.shadowColor = UIColor(red:0, green:0, blue:0, alpha:0.17)
+			.withAlphaComponent(0.3)
+			.cgColor
 		self.layer.shadowOpacity = 0.6
-		self.layer.shadowRadius = 2
+		self.layer.shadowRadius = 0.7
 		self.setImage(UIImage(named: "clear_search"), for: .clear, state: .normal)
+		self.setImage(UIImage(), for: .search, state: .normal)
 		
 		if #available(iOS 13.0, *) {
 			self.searchTextField.backgroundColor = UIColor(named: "CategoryPickerHeaderBackground")
