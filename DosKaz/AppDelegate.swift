b18dc9876c3714fd6d0e7d8dfd893f3cb070ef9b
Yesbol Kulanbekov <esbolkz@yahoo.com>
2020-05-06 14:35:05 +0600

    configure filter table view

--- DosKaz/AppDelegate.swift
+++ DosKaz/AppDelegate.swift
@@ -14,7 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		setMockViewController(MainTabBarViewController())
+		setMockViewController(
+			UINavigationController(
+				rootViewController: FilterBuilder().assembleModule())
+			)
 		return true
 	}
 	
--- DosKaz/Modules/Filter/View/FilterViewController.swift
+++ DosKaz/Modules/Filter/View/FilterViewController.swift
@@ -28,14 +28,19 @@ extension FilterViewController: FilterViewInput {
 			target: self,
 			action: #selector(closeFilterForm)
 		)
+		configureTableView()
 	}
 		
 	func updateForm(with filter: Filter) {
 		self.filter = filter
+		updateFirstSectionData()
+		updateSecondSectionData()
 	}
 	
 	func makeForm(with filter: Filter) {
 		self.filter = filter
+		updateFirstSectionData()
+		updateSecondSectionData()
 	}
 	
 	@objc func closeFilterForm() {
@@ -47,7 +52,7 @@ extension FilterViewController: FilterViewInput {
 class FilterViewController: UIViewController {
 
 	var output: FilterViewOutput!
-	
+	let tableView = UITableView(frame: .zero, style: .plain)
 	var onRightButtonTouch: CommandWith<OverallScore> = .nop
 	
 	var filter = Filter() {
@@ -61,5 +66,74 @@ class FilterViewController: UIViewController {
 		super.viewDidLoad()
 		output.viewIsReady()
 	}
+	
+	// MARK: - Private fields
+	
+	var sectionOneDataSource: TableViewDataSource<BasicCell.Props, BasicCell>!
+	var sectionTwoDataSource: TableViewDataSource<BasicCell.Props, BasicCell>!
+	var tableViewDataSource: SectionedTableViewDataSource!
+	
+	private func configureTableView() {
+		tableView.tableFooterView = UIView()
+		
+		/// Data sources
+		sectionOneDataSource = TableViewDataSource(tableView) { $1.props = $0 }
+		sectionTwoDataSource = TableViewDataSource(tableView) { $1.props = $0 }
+		tableViewDataSource = SectionedTableViewDataSource(dataSources: [sectionOneDataSource, sectionTwoDataSource])
+		tableView.dataSource = tableViewDataSource
+		
+		/// Table view layout
+		view.addSubview(tableView)
+		tableView.addConstraintsProgrammatically
+		.pinToSuperSafeArea()
+	}
 
+	private func updateFirstSectionData() {
+		
+		let cellOne = BasicCell.Props(
+			text: l10n(.accessibleFull),
+			icon: Asset.local("available_32"),
+			rightIcon: filter.icon(.fullAccessible),
+			onRightButtonTouch: CommandWith {
+				self.onRightButtonTouch.perform(with: .fullAccessible)
+			}
+		)
+		let cellTwo = BasicCell.Props(
+			text: l10n(.accessiblePartial),
+			icon: Asset.local("partially_available_32"),
+			rightIcon: filter.icon(.partialAccessible),
+			onRightButtonTouch: CommandWith {
+				self.onRightButtonTouch.perform(with: .partialAccessible)
+			}
+		)
+		
+		let cellThree = BasicCell.Props(
+			text: l10n(.accessibleNone),
+			icon: Asset.local("not_available_32"),
+			rightIcon: filter.icon(.notAccessible),
+			onRightButtonTouch: CommandWith {
+				self.onRightButtonTouch.perform(with: .notAccessible)
+			}
+		)
+		
+		sectionOneDataSource.cellsProps = [cellOne, cellTwo, cellThree]
+		tableView.reloadData()
+	}
+	
+	private func updateSecondSectionData() {
+		let cells = filter.cat.map { categ in
+			return BasicCell.Props(
+				text: categ.title,
+				icon: Asset.fontAwesome(categ.icon),
+				rightIcon: "chevron_right_passive",
+				onRightButtonTouch: CommandWith {
+					print("Cate is chosen")
+				}
+			)
+		}
+		
+		sectionTwoDataSource.cellsProps = cells
+		tableView.reloadData()
+	}
+	
 }
