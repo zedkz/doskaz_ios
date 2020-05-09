d334a5c892eb26cf80d44dbc3d25368195d6e41a
Yesbol Kulanbekov <esbolkz@yahoo.com>
2020-05-09 18:38:32 +0600

    load disabilityCategories

--- DosKaz/AppDelegate.swift
+++ DosKaz/AppDelegate.swift
@@ -14,9 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		setMockViewController(
-			MainTabBarViewController()
-		)
+//		setMockViewController(
+//			MainTabBarViewController()
+//		)
+		setRootViewController()
 		return true
 	}
 	
---
+++ DosKaz/Services/Doskaz/local_disability_categories.swift
@@ -0,0 +1,37 @@
+//
+//  local_disability_categories.swift
+//  DosKaz
+//
+//  Created by Yesbol Kulanbekov on 5/9/20.
+//  Copyright © 2020 zed. All rights reserved.
+//
+
+import Foundation
+
+struct DisabilityCategories {
+	func load() -> [Disability] {
+		guard let url = Bundle.main.url(forResource: "disabilityCategories", withExtension: "json") else {
+			print("There is no disabilityCategories in the bundle")
+			return []
+		}
+		guard let data = try? Data(contentsOf: url) else {
+			fatalError("Failed to load disabilityCategories.json from bundle.")
+		}
+		
+		let decoder = JSONDecoder()
+		
+		do {
+			let disabilities = try decoder.decode([Disability].self, from: data)
+			return disabilities
+		} catch {
+			fatalError("DisabilityCategories decoding failed:\(error.localizedDescription)")
+		}
+	}
+	
+}
+
+// MARK: - DisabilityElement
+struct Disability: Codable {
+	let key, title, categoryForAPI: String
+}
+
