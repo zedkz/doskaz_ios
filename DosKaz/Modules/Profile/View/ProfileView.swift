449493ddd8d0c50b025d384b02a0310478fd7003
Yesbol Kulanbekov <esbolkz@yahoo.com>
2020-05-18 16:37:57 +0600

    add levelsContainer

--- DosKaz/Modules/Profile/View/ProfileView.swift
+++ DosKaz/Modules/Profile/View/ProfileView.swift
@@ -28,6 +28,11 @@ class ProfileView: UIView {
 		title.backgroundColor = .yellow
 		title.text = "kudaibergenov almas"
 		statusTitle.text = "almos gino enis kero fnec kops lojf aesc twnc dofe alsk adsf asdf zxcv esld esfs lopf ersd erds fdtr gfbv jknm vbfg dfsd"
+		levelProgressLabel.text = "34/56"
+		levelLabel.text = "7 level"
+		levelProgress.setProgress(0.3, animated: true)
+		levelsStats.text = "18 объектов     5 проверок"
+		levelsStats.backgroundColor = .systemRed
 		//fake end
 		
 		editButton.setTitle(l10n(.edit), for: .normal)
@@ -47,6 +52,7 @@ class ProfileView: UIView {
 		
 		mainStack.addArrangedSubview(mainInfoContainer)
 		mainStack.addArrangedSubview(editButton)
+		mainStack.addArrangedSubview(levelsContainer)
 		
 		mainInfoContainer.addSubview(avatarImageView)
 		mainInfoContainer.addSubview(title)
@@ -71,6 +77,24 @@ class ProfileView: UIView {
 		
 		editButton.addConstraintsProgrammatically
 			.set(my: .height, to: 40)
+		
+		levelsContainer.addSubview(levelLabel)
+		let levelProgressStack = horStack(progressView: levelProgress, label: levelProgressLabel)
+		levelsContainer.addSubview(levelProgressStack)
+		levelsContainer.addSubview(levelsStats)
+		
+		levelLabel.addConstraintsProgrammatically
+			.pinEdgeToSupers(.top)
+			.pinEdgeToSupers(.leading)
+			.pin(my: .trailing, to: .leading, of: levelProgressStack)
+		levelProgressStack.addConstraintsProgrammatically
+			.pinEdgeToSupers(.trailing)
+			.pinEdgeToSupers(.top)
+		levelsStats.addConstraintsProgrammatically
+			.pinEdgeToSupers(.bottom)
+			.pinEdgeToSupers(.trailing)
+			.pinEdgeToSupers(.leading)
+			.pin(my: .top, to: .bottom, of: levelLabel, plus: 8)
 	}
 	
 	//MARK: - Public properties and methods
@@ -83,6 +107,13 @@ class ProfileView: UIView {
 	//2
 	let editButton = Button(type: .system)
 	
+	//3
+	let levelsContainer = UIView()
+	let levelLabel = UILabel()
+	let levelProgressLabel = UILabel()
+	let levelProgress = UIProgressView(progressViewStyle: .default)
+	let levelsStats = UILabel()
+	
 	let mainStack: UIStackView = {
 		let s = UIStackView()
 		s.axis = .vertical
@@ -91,6 +122,22 @@ class ProfileView: UIView {
 		s.layoutMargins = UIEdgeInsets(all: 10)
 		return s
 	}()
+	
+	func horStack(progressView: UIProgressView, label: UIView) -> UIStackView {
+		let s = UIStackView()
+		s.axis = .horizontal
+		s.alignment = .center
+		s.spacing = 8
+		progressView.addConstraintsProgrammatically
+			.set(my: .width, to: 80)
+			.set(my: .height, to: 3)
+		progressView.layer.cornerRadius = 3/2
+		progressView.clipsToBounds = true
+		progressView.trackTintColor = UIColor(named: "UnselectedTabbarTintColor")?.withAlphaComponent(0.3)
+		s.addArrangedSubview(progressView)
+		s.addArrangedSubview(label)
+		return s
+	}
 	
 	//MARK: - Sub types
 	
