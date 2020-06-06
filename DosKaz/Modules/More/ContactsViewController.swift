e8b91f11d68835ea6919b3bb2265c31d685d50cb
Yesbol Kulanbekov <esbolkz@yahoo.com>
2020-06-06 17:04:52 +0600

    add ContactsViewController

---
+++ DosKaz/Modules/More/ContactsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ContactsViewController.swift
+//  DosKaz
+//
+//  Created by Yesbol Kulanbekov on 6/6/20.
+//  Copyright © 2020 zed. All rights reserved.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+	
+	
+}
