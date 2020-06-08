4c790f44678c5cf84f8837ea8facaa329c2ecde4
Yesbol Kulanbekov <esbolkz@yahoo.com>
2020-06-08 15:47:49 +0600

    add IQKeyboardManager

--- DosKaz/AppDelegate.swift
+++ DosKaz/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		IQKeyboardManager.shared.enable = true
 		setMockViewController(MainTabBarViewController())
 		return true
 	}
