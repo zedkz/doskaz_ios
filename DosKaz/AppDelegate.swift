ecb7d8b34bb18d10e809582387b64e476d0e6ceb
Yesbol Kulanbekov <esbolkz@yahoo.com>
2020-06-11 13:32:52 +0600

    Contacts: add main stack

--- DosKaz/AppDelegate.swift
+++ DosKaz/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		IQKeyboardManager.shared.enable = true
-		setMockViewController(MainTabBarViewController())
+		setMockViewController(ContactsViewController())
 		return true
 	}
 	
--- DosKaz/Modules/More/ContactsViewController.swift
+++ DosKaz/Modules/More/ContactsViewController.swift
@@ -12,7 +12,29 @@ class ContactsViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
+		view.backgroundColor = UIColor.white
+		configureLayout()
+	}
+	
+	let scrollView = UIScrollView()
+	
+	var contentView: UIStackView = {
+		let stack = UIStackView()
+		stack.axis = .vertical
+		stack.spacing = 10
+		stack.isLayoutMarginsRelativeArrangement = true
+		stack.layoutMargins = UIEdgeInsets(top: 43, left: 20, bottom: 20, right: 20)
+		return stack
+	}()
+	
+	private func configureLayout() {
+		view.addSubview(scrollView)
+		scrollView.addConstraintsProgrammatically
+			.pinToSuperSafeArea()
+		scrollView.addSubview(contentView)
+		contentView.addConstraintsProgrammatically
+			.pinToSuper()
+			.set(my: .width, .equal, to: .width, of: scrollView)
 	}
 	
 	
