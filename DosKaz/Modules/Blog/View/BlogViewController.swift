cbbb7149d4fed8bea299e8034ce27939c41a389b
Yesbol Kulanbekov <esbolkz@yahoo.com>
2020-06-14 18:02:45 +0600

    add scroll view

--- DosKaz/Modules/Blog/View/BlogViewController.swift
+++ DosKaz/Modules/Blog/View/BlogViewController.swift
@@ -18,7 +18,29 @@ class BlogViewController: UIViewController, BlogViewInput {
 
 	var output: BlogViewOutput!
 	
+	let scrollView = UIScrollView()
+	
+	var contentView: UIStackView = {
+		let stack = UIStackView()
+		stack.axis = .vertical
+		stack.spacing = 0
+		stack.isLayoutMarginsRelativeArrangement = true
+		stack.layoutMargins = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+		return stack
+	}()
+	
 	func setupInitialState() {
+		configureLayout()
+	}
+	
+	private func configureLayout() {
+		view.addSubview(scrollView)
+		scrollView.addConstraintsProgrammatically
+			.pinToSuperSafeArea()
+		scrollView.addSubview(contentView)
+		contentView.addConstraintsProgrammatically
+			.pinToSuper()
+			.set(my: .width, .equal, to: .width, of: scrollView)
 		
 	}
 }
